64f4521979a146de69252c3eb081724a5037b42f
Clarence Mills <clarence.mills@gmail.com>
2019-01-15 23:07:12 -0500

    New function to obtain password from the user

--- crypt.py
+++ crypt.py
@@ -6,16 +6,23 @@ import pyAesCrypt
 buffersize = 64 * 1024
 password = "foobar"
 
+def get_user_pass():
+    user_pasword = input('Enter password for encrpting: ')
+    return 'user_password'
+    
+
 # encrypt Function
 def encrypt(file):
+    password = get_user_pass()
     encrypted_file_name = file + ".aes"
     print("Encrypting file: " + file)
     pyAesCrypt.encryptFile(file,encrypted_file_name, password, buffersize)
 
 # Decrypt function
 def decrypt(file):
+    user_password = input('Enter password to be used for deencryption: ')
     print("Decrypting file:" + file)
-    pyAesCrypt.decryptFile(file, "test_data.unc", password, buffersize)
+    pyAesCrypt.decryptFile(file, "test_data.unc", user_password, buffersize)
 
 # Start program
 if int(len(sys.argv)) == 1:
